6e0b56e100e538dae4055d95e8c7b6933398cf28
christian mpimbaza <xtianm4@gmail.com>
2018-08-29 11:30:31 +0300

    added numbers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Storage} from "@ionic/storage";
+import {BrokerDetailPage} from "../broker-detail/broker-detail";
 
 
 @IonicPage()
@@ -10,7 +11,12 @@ import {Storage} from "@ionic/storage";
 })
 export class HomePage {
   viewMode: string = "home";
-  slideData = [{ image: "assets/img/slider5.jpg" },{ image: "assets/img/slide6.jpg" },{ image: "assets/img/slide7.jpg" },{ image: "assets/img/slide8.jpg" }];
+  slideData = [
+    { image: "assets/img/slider5.jpg", merchant_name: "KFC", merchant_code: "12345", m_business_type: "Restaurant", m_location: "Lugogo", contact: "0312532532"},
+    { image: "assets/img/slide6.jpg", merchant_name: "MTN", merchant_code: "12345", m_business_type: "Telcom", m_location: "Shoprite", contact: "123"},
+    { image: "assets/img/slide7.jpg", merchant_name: "TAG TEAM", merchant_code: "12345", m_business_type: "Pizza", m_location: "Lugogo", contact: "0800273030"},
+    { image: "assets/img/slide8.jpg", merchant_name: "Plater", merchant_code: "12345", m_business_type: "Motel", m_location: "Lugogo", contact: "256706111110"}
+    ];
   homeOptions = {
     initialSlide: 0,
     loop: true,
@@ -28,7 +34,9 @@ export class HomePage {
     //What should i do in this method?
 
   };
-
+  openSpeakerDetail(merchant) {
+    this.navCtrl.push(BrokerDetailPage, merchant);
+  }
   goToMyPage(page:string) {
     // go to the MyPage component
     this.navCtrl.push(page);
